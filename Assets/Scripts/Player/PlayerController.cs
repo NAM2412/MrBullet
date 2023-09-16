e774fa5979c4ec379f5c24abc1f572d1677bd81c
NAM2412 <20520643@gm.uit.edu.vn>
2023-09-16 13:38:16 +0700

    Making bullet

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float rotateSpeed = 100f, bulletSpeed = 100f, timeToDestroyBullet = 2f;
+
+    private Transform handPos;
+    private Transform fireStartPos, fireEndPos;
+
+    private LineRenderer lineRenderer; // the laser in game
+
+    private void Awake()
     {
-        
+        handPos = GameObject.Find("LeftArm").transform;
+        fireStartPos = GameObject.Find("FirePos1").transform;
+        fireEndPos = GameObject.Find("FirePos2").transform;
+        lineRenderer = GameObject.Find("Gun").GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Aim();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Shoot();
+        }
+
     }
+
+    private void Aim()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - handPos.position;
+        float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg + 90;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward); // rotate the hand holds gun.
+        handPos.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed + Time.deltaTime);
+
+
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, fireStartPos.position);
+        lineRenderer.SetPosition(1, fireEndPos.position);
+    }
+
+    private void Shoot()
+    {
+        lineRenderer.enabled = false;
+
+        GameObject bullet = Instantiate(bulletPrefab, fireStartPos.position, Quaternion.identity);
+
+        if (transform.localScale.x > 0)
+        {
+            bullet.GetComponent<Rigidbody2D>().AddForce(fireStartPos.right * bulletSpeed, ForceMode2D.Impulse);
+        }
+        else
+        {
+            bullet.GetComponent<Rigidbody2D>().AddForce(-fireStartPos.right * bulletSpeed, ForceMode2D.Impulse);
+        }    
+        Destroy(bullet, timeToDestroyBullet);
+    }
+
 }
